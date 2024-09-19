cb0985788566ff32c41974b55692b8e5df093c04
vidya rautela <vidya.rautela.28@gmail.com>
2024-09-20 01:07:47 +0530

    Added: created an IAM user, modified it and print op in console

--- Terraform/terraform-basics/main.tf
+++ Terraform/terraform-basics/main.tf
@@ -24,7 +24,25 @@ output "aws_s3_bucket_versioning"  {
 }
 output "aws_s3_bucket_complete_details" {
   value = aws_s3_bucket.tf-name-s3-bucket
-  sensitive=true
+  # sensitive=true
 }
 
+# created an iam user and tf was able to recognize that i didn't touch s3 bucket just added iam resource block to create a user.
 
+# using resource, create a new iam user with reference name my_aws_iam_user and actual name in cloud as dev-iam-user
+resource "aws_iam_user" "my_aws_iam_user" {
+  name = "dev-iam-user-updated"
+}
+
+# fetch existing IAM user and display the output. ( using datasource )
+data "aws_iam_user" "existing_iam_user" {
+  user_name = "command_line_user"
+}
+# this is similar as of output in programming
+output "iam_user_name_complete_details" {
+  value = data.aws_iam_user.existing_iam_user
+}
+
+# output "iam_user_name_complete_details-1"{
+#   value= aws_iam_user.existing_iam_user.user_name
+# }

33c8a2ba04848d9a822aced571a62858de26f1c5
vidya rautela <vidya.rautela.28@gmail.com>
2024-09-22 22:38:07 +0530

    modfied files

---
+++ Terraform/02-Terraform-basics/main.tf
@@ -0,0 +1,14 @@
+variable "iam_user_name_prefix" {
+  # type        = string
+  default     = "my_iam_user"
+  # description = "description"
+}
+
+
+provider "aws" {
+}
+resource "aws_iam_user" "my_aws_iam_users" {
+  # using tf hcl lang i can declare the no of user i want and terraform will check the state if the user is already present and increase the count by 1 then it will only create that user while keeping the desrired state. 
+  count = 3
+  name  = "${var.iam_user_name_prefix}_${count.index}"
+}

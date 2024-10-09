991c5ff124ac955ee6e17ab183e0b78bc412b046
vidya rautela <vidya.rautela.28@gmail.com>
2024-10-09 23:45:33 +0530

    modified: added egress block

--- Terraform/05-EC2-Instance/main.tf
+++ Terraform/05-EC2-Instance/main.tf
@@ -8,3 +8,18 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+resource "aws_security_group" "http_server_sg" {
+  name="http_server_sg"
+  vpc_id = aws_vpc.example.id
+
+
+
+
+    egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
